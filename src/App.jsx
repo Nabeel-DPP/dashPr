33b8a764e558b16394f4224254be8159d764d62e
Nabeel Hussain <testmis@infosys.net>
2024-09-20 16:53:55 +0500

    Dashboard Project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
+import { Home } from "./Home";
+import React from 'react'
+ 
+ export const App = () => {
+   return (
+    <Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        {/* <Route path="about" element={<About />} />
+        <Route path="services" element={<Services />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="table" element={<ReactTable />} /> */}
+
+      </Route>
+    </Routes>
+  </Router>
+   )
+ }
+ 
---
+++ src/layout/Footer.jsx
@@ -0,0 +1,16 @@
+
+import "./Layout.css";
+
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div> <footer className="footer mt-auto">
+    <div className="copyright bg-white">
+      <p>
+        &copy; <span id="copy-year"></span> Copyright by Raazee Limited  <a className="text-primary" href="http://www.iamabdus.com/" target="_blank" >Kasur</a>.
+      </p>
+    </div>
+   </footer></div>
+  )
+}
